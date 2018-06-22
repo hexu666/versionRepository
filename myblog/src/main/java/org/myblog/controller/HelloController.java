34e9f01e0ea923f9daf5c25798c3cde8807c4b04
hexu666 <13823166200@163.com>
2018-06-23 00:23:20 +0800

    commit 20180623

--- myblog/src/main/java/org/myblog/controller/HelloController.java
+++ myblog/src/main/java/org/myblog/controller/HelloController.java
@@ -1,5 +1,7 @@
 package org.myblog.controller;
 
 public class HelloController {
-
+	   public static void main(String[] args) {
+		System.out.println("Hello");
+	}
 }
--- myblog/src/test/java/org/myblog/controller/HelloControllerTest.java
+++ myblog/src/test/java/org/myblog/controller/HelloControllerTest.java
@@ -1,5 +1,7 @@
 package org.myblog.controller;
 
 public class HelloControllerTest {
-
+	public static void main(String[] args) {
+		System.out.println("Hello");
+	}
 }
