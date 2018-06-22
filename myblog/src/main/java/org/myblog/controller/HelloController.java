ac2dcc8dc74be766d632c022f9a907df4afb178c
hexu666 <13823166200@163.com>
2018-06-23 01:40:26 +0800

    commit update test

--- myblog/src/main/java/org/myblog/controller/HelloController.java
+++ myblog/src/main/java/org/myblog/controller/HelloController.java
@@ -3,5 +3,8 @@ package org.myblog.controller;
 public class HelloController {
 	   public static void main(String[] args) {
 		System.out.println("Hello");
+		System.out.println("Hello");
+		System.out.println("Hello");
+		
 	}
 }
