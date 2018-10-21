15e9f97c100dcaad6e0519e3928c726a33444337
hexu666 <13823166200@163.com>
2018-10-22 02:08:22 +0800

    first_vue code test

---
+++ proj/vue-book-master/first_vue/src/BlogIndex.vue
@@ -0,0 +1,23 @@
+<template>
+  <!--https://segmentfault.com/a/1190000014211773#articleHeader0-->
+  <div>
+    <blog-header></blog-header>
+    <hr/>
+    <div>
+      这是首页，嘻嘻嘻。
+    </div>
+    <hr/>
+    <blog-footer></blog-footer>
+  </div>
+</template>
+ 
+<script>
+  import blogHeader from '@/components/common/BlogHeader.vue'
+  import blogFooter from '@/components/common/BlogFooter.vue'
+
+  export default {
+    name: 'BlogIndex',
+    // blogHeader/blogFooter组件给申明到components里面然后在template里面使用
+    components: { blogHeader, blogFooter }
+  }
+</script>
---
+++ proj/vue-book-master/first_vue/src/utils/utils.js
@@ -0,0 +1,18 @@
+//utils.js
+
+import axios from 'axios'
+
+const utils = {
+
+    axiosMethod: (config) => {
+      axios({
+          method: config.method;
+          url: config.url,
+          params ：config.params ? config.params :null,
+          data ：config.data ? config.data :null,
+      }).then(config.callback).catch(config.catch ? config.catch :() =>{})
+
+    }
+}
+
+
